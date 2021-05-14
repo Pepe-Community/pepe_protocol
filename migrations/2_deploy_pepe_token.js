b8c101bb22184e1865a86c74cec260d61aa6879c
Tam Nguyen <nntam2013@gmail.com>
2021-05-14 16:47:33 +0700

    chore: update migration script

--- migrations/2_deploy_pepe_token.js
+++ migrations/2_deploy_pepe_token.js
@@ -4,8 +4,6 @@ const Utils = artifacts.require("Utils");
 module.exports = async function (deployer) {
   await deployer.deploy(Utils);
   await deployer.link(Utils, PepeToken);
-  await deployer.deploy(
-    PepeToken,
-    "0xD99D1c33F9fC3444f8101754aBC46c52416550D1"
-  );
+  const PANCAKE_ROUTER = "0xD99D1c33F9fC3444f8101754aBC46c52416550D1";
+  await deployer.deploy(PepeToken, PANCAKE_ROUTER);
 };
